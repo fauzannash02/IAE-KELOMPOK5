689b0bc63824c50632b6b8748ac06d76f6137787
Muhamad Ivan Fadilah <muhammadivan174@gmail.com>
2022-06-22 16:32:48 +0700

    fix migration pelanggan

--- pelanggan/database/migrations/2022_06_21_071620_create_pelanggans_table.php
+++ pelanggan/database/migrations/2022_06_21_071620_create_pelanggans_table.php
@@ -21,6 +21,7 @@ return new class extends Migration
             $table->string('asal_daerah');
             $table->string('no_hp');
             $table->string('email');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
